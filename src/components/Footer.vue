267a43cd4aa64f1f771548ad926bb55cea7f4224
Everaert Robin <r-everaert@outlook.fr>
2025-01-05 18:25:04 +0100

    Footer developed

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer id="footer" class="footer bg-dark text-white text-center py-4">
+        <div class="container">
+        <p class="mb-0">© {{ currentYear }} Everaert Robin. All rights reserved.</p>
+        <div class="mt-4">
+            <a href="https://www.linkedin.com/in/robin-everaert/" target="_blank" class="text-white mx-2">
+            <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/robin-everaert" target="_blank" class="text-white mx-2">
+            <i class="fab fa-github"></i>
+            </a>
+        </div>
+        </div>
+    </footer>
+  </template>
+  <script>
+  export default {
+    name: 'Footer',
+    setup() {
+        const currentYear = new Date().getFullYear(); 
+        return {
+        currentYear,
+        };
+    },
+  };
+  </script>
+
+<footer class="bg-dark text-white text-center py-4">
+    
+  </footer>
+  
